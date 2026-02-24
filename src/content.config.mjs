7e8d3168f2c817d2dcda7de48a6b16816e175e32
Clark Sell <clark.sell@csell.net>
2026-02-24 12:57:13 -0600

    thesis page 2

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -1,23 +1,23 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection } from "astro:content";
 
 // Define all content collections
 const collections = {
   // Existing collections (auto-discovered)
-  IndexPage: defineCollection({}),
-  Pricing: defineCollection({}),
-  aboutPage: defineCollection({}),
-  apply: defineCollection({}),
-  blogs: defineCollection({}),
-  career: defineCollection({}),
-  comingSoon: defineCollection({}),
-  contact: defineCollection({}),
-  gettingStarted: defineCollection({}),
-  notFound: defineCollection({}),
-  portfolio: defineCollection({}),
-  privacyPolicy: defineCollection({}),
-  services: defineCollection({}),
-  site: defineCollection({}),
-  termsCondition: defineCollection({}),
+  index: defineCollection({ type: "content" }),
+  Pricing: defineCollection({ type: "content" }),
+  aboutPage: defineCollection({ type: "content" }),
+  apply: defineCollection({ type: "content" }),
+  blogs: defineCollection({ type: "content" }),
+  career: defineCollection({ type: "content" }),
+  comingSoon: defineCollection({ type: "content" }),
+  contact: defineCollection({ type: "content" }),
+  gettingStarted: defineCollection({ type: "content" }),
+  notFound: defineCollection({ type: "content" }),
+  portfolio: defineCollection({ type: "content" }),
+  privacyPolicy: defineCollection({ type: "content" }),
+  services: defineCollection({ type: "content" }),
+  site: defineCollection({ type: "content" }),
+  termsCondition: defineCollection({ type: "content" }),
 };
 
 export { collections };
